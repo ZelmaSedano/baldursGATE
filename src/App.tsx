68f7c3e3862fe4e933521c72cd5f53711e6b27a4
ZelmaSedano <zvsedano@gmail.com>
2025-11-01 07:54:06 -0500

    changing text

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,15 @@ function App() {
     <>
       <div>
       </div>
-      <h1 className='header'>Vite + React</h1>
+      <h1 className='header'>Chris is a femboy</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          z
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
